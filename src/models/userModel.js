5b5b9ee749f7db8628799c1b2ad68ed54c6b4889
gmeline <melinegodefroy.pr@gmail.com>
2025-06-19 14:50:56 +0200

    Ajout de id dans le model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
+    id: { type: Number, required: true },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     age: { type: Number },
